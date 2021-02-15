36a44128dce0670a022c590adb283197d991a4e8
Alexis3490 <alexispiraux72@gmail.com>
2021-02-15 16:10:08 +0100

    update

--- plugins/dbr.js
+++ plugins/dbr.js
@@ -2,6 +2,6 @@ import DBR from 'dynamsoft-javascript-barcode'
 DBR.BarcodeReader.engineResourcePath =
   'https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@8.1.2/dist/'
 // Please visit https://www.dynamsoft.com/customer/license/trialLicense to get a trial license
-DBR.BarcodeReader.productKeys = 'PRODUCT-KEYS'
+DBR.BarcodeReader.productKeys = 't0076xQAAAFmWbFijBdp64Jugk2YJSnsprwqoF/UiyXqGQyevujt+9PJ52Ctw/sBBsIpL0L8uunjH9M/xLdX4pjvnSe3f96IeBTwWAdgpTw=='
 // DBR.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
 export default DBR
