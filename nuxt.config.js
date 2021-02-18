70a3ab5aaa2bca4f77605a0113421c21c6901d9c
Alexis3490 <alexispiraux72@gmail.com>
2021-02-18 09:43:09 +0100

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,57 +1,57 @@
 export default {
-	mode: 'universal',
-	/*
-	 ** Headers of the page
-	 */
-	head: {
-		title: process.env.npm_package_name || '',
-		meta: [
-			{ charset: 'utf-8' },
-			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
-			{
-				hid: 'description',
-				name: 'description',
-				content: process.env.npm_package_description || '',
-			},
-		],
-		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-		script: [
-			{
-				src:
-					'"https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"',
-			},
-		],
-	},
-	/*
-	 ** Customize the progress-bar color
-	 */
-	loading: { color: '#fff' },
-	/*
-	 ** Global CSS
-	 */
-	css: [],
-	/*
-	 ** Plugins to load before mounting the App
-	 */
-	plugins: [
-		'./api/init.js',
-		{ src: '~/plugins/persistedState.client.js' },
-		{ src: '~/plugins/persistedState.js' },
-	],
-	/*
-	 ** Nuxt.js dev-modules
-	 */
-	buildModules: [],
-	/*
-	 ** Nuxt.js modules
-	 */
-	modules: [
-		'nuxt-logrocket',
-		'bootstrap-vue/nuxt',
-		'@nuxtjs/axios',
-		'mdbvue/nuxt',
-		'@nuxtjs/pwa',
-	],
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || '',
+      },
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    script: [
+      {
+        src:
+          '"https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"',
+      },
+    ],
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [
+    './api/init.js',
+    { src: '~/plugins/persistedState.client.js' },
+    { src: '~/plugins/persistedState.js' },
+  ],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    'nuxt-logrocket',
+    'bootstrap-vue/nuxt',
+    '@nuxtjs/axios',
+    'mdbvue/nuxt',
+    '@nuxtjs/pwa',
+  ],
 
   mdbvue: {
     icons: true, // FA5
