85fda25aa32f082090db9914103d5c60fa6f7ffb
MestrieEsteban <esteban94.em@gmail.com>
2021-02-18 08:32:32 +0100

    ok

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,84 +1,86 @@
 export default {
-  mode: 'universal',
-  /*
-   ** Headers of the page
-   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      {
-        hid: 'description',
-        name: 'description',
-        content: process.env.npm_package_description || '',
-      },
-    ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-    script: [
-      {
-        src:
-          '"https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"',
-      },
-    ],
-  },
-  /*
-   ** Customize the progress-bar color
-   */
-  loading: { color: '#fff' },
-  /*
-   ** Global CSS
-   */
-  css: [],
-  /*
-   ** Plugins to load before mounting the App
-   */
+	mode: 'universal',
+	/*
+	 ** Headers of the page
+	 */
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{
+				hid: 'description',
+				name: 'description',
+				content: process.env.npm_package_description || '',
+			},
+		],
+		link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+		script: [
+			{
+				src:
+					'"https://cdn.lr-ingest.io/LogRocket.min.js" crossorigin="anonymous"',
+			},
+		],
+	},
+	/*
+	 ** Customize the progress-bar color
+	 */
+	loading: { color: '#fff' },
+	/*
+	 ** Global CSS
+	 */
+	css: [],
+	/*
+	 ** Plugins to load before mounting the App
+	 */
 	plugins: [
 		'./api/init.js',
 		{ src: '~/plugins/persistedState.client.js' },
 		{ src: '~/plugins/persistedState.js' },
 	],
-  /*
-   ** Nuxt.js dev-modules
-   */
-  buildModules: [],
-  /*
-   ** Nuxt.js modules
-   */
-  modules: [
-    'nuxt-logrocket',
-    'bootstrap-vue/nuxt',
-    '@nuxtjs/axios',
-    'mdbvue/nuxt',
-  ],
+	/*
+	 ** Nuxt.js dev-modules
+	 */
+	buildModules: [],
+	/*
+	 ** Nuxt.js modules
+	 */
+	modules: [
+		'nuxt-logrocket',
+		'bootstrap-vue/nuxt',
+		'@nuxtjs/axios',
+		'mdbvue/nuxt',
+		'@nuxtjs/pwa',
 
-  bootstrapVue: {
-    icons: true,
-  },
+	],
 
-  mdbvue: {
-    icons: true, // FA5
-    roboto: false, // font Roboto
-    css: true, // MDB CSS
-    bootstrap: true, // Bootstrap CSS
-  },
-  /*
-   ** Build configuration
-   */
+	bootstrapVue: {
+		icons: true,
+	},
 
-  logRocket: {
-    logRocketId: 'efrei/bibliotech',
-    devModeAllowed: false,
-  },
+	mdbvue: {
+		icons: true, // FA5
+		roboto: false, // font Roboto
+		css: true, // MDB CSS
+		bootstrap: true, // Bootstrap CSS
+	},
+	/*
+	 ** Build configuration
+	 */
 
-  axios: {
-    baseURL: 'https://bibliotech-serveur.herokuapp.com/api/',
-  },
+	logRocket: {
+		logRocketId: 'efrei/bibliotech',
+		devModeAllowed: false,
+	},
 
-  build: {
-    /*
-     ** You can extend webpack config here
-     */
-    extend(config, ctx) {},
-  },
+	axios: {
+		baseURL: 'https://bibliotech-serveur.herokuapp.com/api/',
+	},
+
+	build: {
+		/*
+		 ** You can extend webpack config here
+		 */
+		extend(config, ctx) { },
+	},
 }
