daf90fa2cfe025dd589416527acb8e0a46deca75
MestrieEsteban <esteban94.em@gmail.com>
2021-02-19 08:46:33 +0100

    ok

--- components/Maps.vue
+++ components/Maps.vue
@@ -3,16 +3,17 @@
           <b-col style="text-align: center">
             <gmap-map
               :center="center"
-              :zoom="7"
+              :zoom="12"
               style="width: 100%; height: 300px">
               <gmap-marker
                 :key="index"
                 v-for="(m, index) in markers"
                 :position="m.position"
                 :clickable="true"
-                :draggable="true"
+                :draggable="false"
                 @click="center=m.position"
-              ></gmap-marker>
+              >
+			  </gmap-marker>
             </gmap-map>
           </b-col>
         </b-row>
@@ -25,17 +26,23 @@ import Vue from 'vue'
 Vue.use(VueGoogleMaps, {
     load: {
       key: 'AIzaSyD6jhVV7Z0XYuOkUYHe6vLADfVq4t_ErZM',
+	  libraries: 'places',
     }
   });
 
 export default {
     data() {
     return {
-      center: {lat: 10.0, lng: 10.0},
+      center: {lat: 48.807280, lng: 2.571398},
         markers: [{
-          position: {lat: 10.0, lng: 10.0}
+          position: {lat: 48.799590, lng: 2.607510},
+		  name:'qsd'
         }, {
-          position: {lat: 11.0, lng: 11.0}
+          position: {lat: 48.792376, lng:2.558032}
+        }, {
+          position: {lat: 48.811151, lng:2.544197}
+        }, {
+          position: {lat: 48.810921, lng:2.570657}
         }]
     }
   },
